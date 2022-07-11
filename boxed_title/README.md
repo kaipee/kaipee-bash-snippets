00fb01e5d7d93f659af4ff9891463645309dceac
Keith Patton <github@kaipee.co.uk>
2022-07-11 18:40:22 +0100

    moved instructions to README

---
+++ boxed_title/README.md
@@ -0,0 +1,5 @@
+# Example usage
+clear
+printTitle 'This is a title'
+echo "Some other text or script output can be placed between boxed titles."
+printTitle 'Try to keep it alphanumerical'
--- boxed_title/boxed_title.sh
+++ boxed_title/boxed_title.sh
@@ -15,10 +15,3 @@ function printTitle ()
     return 0
   fi
 }
-
-
-# Example usage
-clear
-printTitle 'This is a title'
-echo "Some other text or script output can be placed between boxed titles."
-printTitle 'Try to keep it alphanumerical'
